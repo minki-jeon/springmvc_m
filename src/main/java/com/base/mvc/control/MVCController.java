b1dbf5c65a08a21cf76ff782f7c87110fdf35354
minki-jeon <minki.jeon.study@gmail.com>
2024-11-14 17:58:24 +0900

    Add @Autowired, @Service

--- src/main/java/com/base/mvc/control/MVCController.java
+++ src/main/java/com/base/mvc/control/MVCController.java
@@ -1,19 +1,17 @@
 package com.base.mvc.control;
 
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.base.mvc.service.MVCService;
-import com.base.mvc.service.impl.MVCServiceImpl;
 
 @Controller
 public class MVCController {
 	
-//	@Autowired
-//	MVCService mvcservice;
-	MVCService mvcservice = new MVCServiceImpl();
+	@Autowired
+	MVCService mvcservice;
 	
 	@RequestMapping("/index.do")
 	public String methodCtl(Model model) throws Exception {
--- src/main/java/com/base/mvc/service/impl/MVCServiceImpl.java
+++ src/main/java/com/base/mvc/service/impl/MVCServiceImpl.java
@@ -1,15 +1,16 @@
 package com.base.mvc.service.impl;
 
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.base.mvc.dao.MVCDao;
 import com.base.mvc.service.MVCService;
 
+@Service
 public class MVCServiceImpl implements MVCService {
 	
-//	@Autowired
-//	private MVCDao mvcdao;
-	MVCDao mvcdao = new MVCDao();
+	@Autowired
+	private MVCDao mvcdao;
 	
 	public String methodSrv() {
 		System.out.println(mvcdao.methodDao());
